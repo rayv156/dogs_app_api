2ba19ec0318bb92ab3af77c196960d0a88f65c81
Ray Velasquez <rayvelasquez@Rays-MacBook-Pro.local>
2020-11-29 21:52:01 -0600

    changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_234444) do
+ActiveRecord::Schema.define(version: 2020_11_30_033449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_234444) do
     t.string "breed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Dog.create(name: "Sparky", age: 5, breed: "Bulldog")
-Dog.create(name: "Spot", age: 5, breed: "Dalmation")
-Dog.create(name: "Goldy", age: 5, breed: "Golden Retriever")
+Dog.create(name: "Sparky", age: 5, breed: "Bulldog", image_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*")
+Dog.create(name: "Spot", age: 5, breed: "Dalmation", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS00aQayxkcwzTmrSN08phiPHunAgzIUQ50aA&usqp=CAU")
+Dog.create(name: "Goldy", age: 5, breed: "Golden Retriever", image_url: "https://thehappypuppysite.com/wp-content/uploads/2017/10/Cute-Dog-Names-HP-long.jpg")
+
+duncan = Dog.find_by(name: 'Duncan')
+duncan.image_url = 'https://hashtaglegend.com/wp-content/uploads/2020/08/international-dog-day-1024x1024.jpg'
+duncan.save
+
+otis = Dog.find_by(name: 'Otis')
+otis.image_url = 'https://iheartdogs.com/wp-content/uploads/2015/01/Screenshot-2015-01-17-16.15.29.png'
+otis.save
+
